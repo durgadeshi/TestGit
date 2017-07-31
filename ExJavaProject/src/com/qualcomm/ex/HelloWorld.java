fed467c9d4145e9075a56215610e8784e7277eca
pkumarni <pkumarni@qti.qualcomm.com>
2017-07-31 11:51:35 +0530

    first commit from pavan

--- ExJavaProject/src/com/qualcomm/ex/HelloWorld.java
+++ ExJavaProject/src/com/qualcomm/ex/HelloWorld.java
@@ -2,6 +2,6 @@ package com.qualcomm.ex;
 
 public class HelloWorld {
 public static void main(String[] args) {
-	System.out.println("Hello Changes");
+	System.out.println("Hello Changes from pavan");
 }
 }
